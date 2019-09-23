c12ed6c64e766edab06956f0737bc6eb4d2dce69
Ahmad Fajar <shin.fajar@gmail.com>
2019-09-23 13:52:55 +0700

    app1 lifecycle

--- app1/index.js
+++ app1/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
 import './index.css';
 
-class App extends React.Component{
-  render(){
-    return(
-      <div>Hello App1 World</div>
-    )
-  }
-}
+const AppRoot = p => {
+  console.log('props: ', p)
+  const s = {
+    color: 'blue',
+    fontSize: '48px',
+    fontWeight: 'bold',
+  };
+  return(
+    <div style={s}>
+      Hello App1 World
+    </div>
+  )
+};
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<AppRoot />, document.getElementById('app'));
+
+const reactLifeCycles = singleSpaReact({
+  React,
+  ReactDOM,
+  domElementGetter,
+  rootComponent: AppRoot,
+});
+
+export const bootstrap = [reactLifeCycles.bootstrap];
+export const mount = [reactLifeCycles.mount];
+export const unmount = [reactLifeCycles.unmount];
+
+function domElementGetter() {
+  const APP_NAME = 'app1';
+  const MOUNT_DIV = 'app';
+
+  let el = document.getElementById(APP_NAME);
+
+  if (!el) {
+    el = document.createElement('div');
+    el.id = APP_NAME;
+    document.body.appendChild(el);
+  }
+
+  let app = document.getElementById(MOUNT_DIV);
+  if (!app) {
+    app = app.createElement('div');
+    app.id = MOUNT_DIV;
+    document.body.appendChild(app);
+  }
+
+  app.appendChild(el);
+
+  return el;
+}
